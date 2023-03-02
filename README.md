67ef4460abee77f4c7e18f0fb23333048b765660
lucas <lucas@nalarocks.com>
2023-03-02 00:04:24 -0300

    build + readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Para iniciar npm run dev
+ingresar a http://localhost:3001/
+api en http://localhost:3001/api/postsv
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/posts', postRouter)
+app.use(express.static('build'))
 
 
 
